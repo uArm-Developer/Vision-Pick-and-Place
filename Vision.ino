0ba2da97e1a01181c381c7d207679e7a12a5f752
unknown <tony@ufactory.cc>
2018-01-16 19:41:06 +0800

    Vision system built with uArm and OpenMV

---
+++ Vision.ino
@@ -0,0 +1,113 @@
+int inByte = 0,//serial buf
+    num = 0;//buf counter
+int x_openmv=0, y_openmv=0;
+int x_uarm=0, y_uarm=0;
+unsigned long times;
+char buf[20],
+     flag=0;
+unsigned char get_openmv_data();
+void pick_and_palce();
+void setup() {
+  Serial.begin(115200);//usb or xbee
+  Serial1.begin(115200);//openmv
+  Serial2.begin(115200);//uarm
+  
+  Serial.write("START!\n");
+  Serial2.write("M2400 S0\n");//set the mode of uarm
+
+  times = millis();
+}
+
+void loop() {
+  if(flag == 0)
+  {
+    Serial2.write("G0 X200 Y0 Z160 F10000\n");
+    delay(5000);//wait for the uarm to finish the moving then start the vision tracking
+    flag = 1;//vision start
+    Serial1.write('S');//send vision start command
+    times = millis();
+  }
+  
+  //get commands from pc
+  if (Serial.available() > 0) 
+  {
+    inByte = Serial.read();
+
+    Serial2.write(inByte);
+  }
+  
+  //get object coordinates from openmv
+  if(get_openmv_data()==1)
+  {
+    x_openmv = 320 - x_openmv;
+    y_openmv = 240 - y_openmv;
+    x_uarm = 200 + (y_openmv - 20)*0.6;//convert the x coordinates for uarm
+    y_uarm = (x_openmv - 125)*0.7;//convert the y coordinates for uarm
+    Serial2.write("G0 X");
+    Serial2.print(x_uarm,DEC);
+    Serial2.write(" Y");
+    Serial2.print(y_uarm,DEC);
+    Serial2.write(" F10000\n");
+Serial.write("x_uarm:");
+Serial.println(x_uarm,DEC);
+Serial.write("y_uarm:");
+Serial.println(y_uarm,DEC);
+    pick_and_palce();
+    //while(1);
+    
+  }
+}
+
+//get object coordinates from openmv
+unsigned char get_openmv_data()
+{
+  if (Serial1.available() > 0) 
+  {
+    inByte = Serial1.read();
+    buf[num++] = inByte;
+    Serial.write(inByte);
+    if((inByte=='\n')&&(buf[0]=='x'))
+    {
+      int counters=1;//jump the letter x
+      x_openmv=0;
+      do{
+        x_openmv = x_openmv*10;
+        x_openmv += buf[counters++] - 48;
+      }while((buf[counters]>=0x30)&&(buf[counters]<=0x39));
+      
+      y_openmv=0;
+      counters++;//jump the letter y
+      do{
+        y_openmv = y_openmv*10;
+        y_openmv += buf[counters++] - 48;
+      }while(counters+1<num);
+
+      num = 0;
+      flag = 0;//vision end
+      return 1;
+    }
+    //Serial.println(x_openmv,DEC);
+    //Serial.println(y_openmv,DEC);
+
+  }
+  if((millis()-times>10000)&&(flag==1))//if no object detected, reset the flag every 10s
+  {
+     times = millis();
+     flag = 0;
+     Serial.write("status 1\n");
+  }
+  return 0;
+}
+//move the detected object to the fixed position
+void pick_and_palce()
+{
+  Serial2.write("G0 Z27 F10000\n");
+  Serial2.write("M2231 V1\n");
+  Serial2.write("G0 Z100 F10000\n");
+  delay(500);
+  Serial2.write("G0 X150 Y-150 Z100 F10000\n");
+  Serial2.write("G0 Z31 F10000\n");
+  Serial2.write("M2231 V0\n");
+  delay(4000);
+}
+
---
+++ color_tracking_test.py
@@ -0,0 +1,91 @@
+# Single Color Code Tracking Example
+#
+# This example shows off single color code tracking using the OpenMV Cam.
+#
+# A color code is a blob composed of two or more colors. The example below will
+# only track colored objects which have both the colors below in them.
+
+import sensor, image, time
+from pyb import UART
+from pyb import LED
+blue_led  = LED(3)
+green_led  = LED(2)
+red_led  = LED(1)
+
+uart = UART(3, 115200, timeout_char = 1000)
+blue_led.on()
+# Color Tracking Thresholds (L Min, L Max, A Min, A Max, B Min, B Max)
+# The below thresholds track in general red/green things. You may wish to tune them...
+thresholds = [(30, 100, 15, 127, 15, 127), # generic_red_thresholds -> index is 0 so code == (1 << 0)
+              (30, 100, -64, -8, -32, 32)] # generic_green_thresholds -> index is 1 so code == (1 << 1)
+# Codes are or'ed together when "merge=True" for "find_blobs".
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time = 2000)
+sensor.set_auto_gain(False) # must be turned off for color tracking
+sensor.set_auto_whitebal(False) # must be turned off for color tracking
+clock = time.clock()
+
+blue_led.off()
+green_led.off()
+red_led.off()
+
+object_x_old=0
+object_y_old=0
+# Only blobs that with more pixels than "pixel_threshold" and more area than "area_threshold" are
+# returned by "find_blobs" below. Change "pixels_threshold" and "area_threshold" if you change the
+# camera resolution. "merge=True" must be set to merge overlapping color blobs for color codes.
+
+while(True):
+    clock.tick()
+
+    blue_led.off()
+    green_led.off()
+    red_led.off()
+
+    img = sensor.snapshot()
+    for blob in img.find_blobs(thresholds, pixels_threshold=100, area_threshold=100, merge=False):
+        if blob.code() == 1: # r/g code == (1 << 1) | (1 << 0)
+
+            img.draw_rectangle(blob.rect())
+            img.draw_cross(blob.cx(), blob.cy())
+            print(blob.cx(), blob.cy(),blob.w())
+
+#make sure the detected object is stable and print the coordinates
+#first it detect if the coordinates of blob is available
+#second compared with the last position to make sure if the object is not moving
+#third reduce the affect of anbience
+            if blob.cx()!=None and (
+                abs(object_x_old - int(blob.cx())) < 4 and
+                abs(object_y_old - int(blob.cy())) < 4) and (
+                blob.w()>35 and
+                blob.h()>35):
+               #just detect the objects. turn on the blue only
+               blue_led.on()
+               red_led.off()
+               green_led.off()
+               print("stable!")
+#check if the uart got any command and response
+               if uart.any()>0 :
+                    buf=uart.read()
+                    if buf==b'S' :
+                        #detect both the objects and the vision command from mega2560. turn on the red only
+                        blue_led.off()
+                        red_led.on()
+                        green_led.off()
+
+                        uart.write('x'+str(blob.cx())+'y'+str(blob.cy())+'\n')
+
+                        #finish the sending. turn on the green only
+                        blue_led.off()
+                        red_led.off()
+                        green_led.on()
+
+
+            object_x_old = int(blob.cx())
+            object_y_old = int(blob.cy())
+
+
+
